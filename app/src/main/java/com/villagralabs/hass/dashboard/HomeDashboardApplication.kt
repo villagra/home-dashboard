83b8afb887f5ce308dc3c16056205b8ca568ae5a
villagra <villagra@gmail.com>
2023-10-01 13:57:57 +0200

    initial

---
+++ app/src/main/java/com/villagralabs/hass/dashboard/HomeDashboardApplication.kt
@@ -0,0 +1,8 @@
+package com.villagralabs.hass.dashboard
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class HomeDashboardApplication : Application() {
+}
